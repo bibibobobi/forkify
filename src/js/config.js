19bd30c62bda4e1e8ed433185d9c6cb63e4ae873
bibibobobi <smilingcat33@gmail.com>
2021-09-07 13:47:45 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'db7683c3-ce73-4a04-ba1c-5d77c999fd78';
+export const MODAL_CLOSE_SEC = 2.5;
